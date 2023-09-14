5ab7fa2649c6185024601376b3b774b08fc15a7b
Muhammad Hamza Munir <hamzamuniransari@gmail.com>
2023-09-14 13:05:25 +0500

    update

--- storage/framework/views/ad30fdc7abf02fc6be7f42ba06b58905.php
+++ storage/framework/views/ad30fdc7abf02fc6be7f42ba06b58905.php
@@ -325,22 +325,10 @@
 
 <!-- Appointment Section -->
 <section class="appointment-section">
-    <div class="image-layer" style="background-image: url(images/background/2.jpg);"></div>
+    <div class="image-layer" style="background-image: url<?php echo e(asset('assets/images/background/2.jpg')); ?>;"></div>
     <div class="auto-container">
-        <div class="row">
-            <!-- Content Column -->
-            <div class="content-column col-lg-6 col-md-12 col-sm-12">
-                <div class="inner-column">
-                    <span class="title">Need a Doctor for Check-up?</span>
-                    <h2>Just Make an Appointment <br>and You’re Done!</h2>
-                    <div class="number">Get Your Quote or Call: <strong>(0080) 123-453-789</strong></div>
-                    <a href="#" class="theme-btn btn-style-three"><span class="btn-title">Get an Appointment</span></a>
-                </div>
-            </div>
-            <div class="image-column col-lg-6 col-md-12 col-sm-12">
-                <figure class="image"><img src="images/resource/image-4.png" alt=""></figure>
-            </div>
-        </div>
+        
+            
 
         <div class="fun-fact-section">
             <div class="row">
