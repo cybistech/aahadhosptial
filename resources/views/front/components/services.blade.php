92444b234b028bd920793e999f1e1f8d12d20b05
Muhammad Hamza Munir <hamzamuniransari@gmail.com>
2023-09-15 12:07:17 +0500

    update

---
+++ resources/views/front/components/services.blade.php
@@ -0,0 +1,8 @@
+<div class="service-block col-lg-4 col-md-6 col-sm-12">
+    <div class="inner-box">
+        {{-- <span class=""><img src="{{asset('upload/service').'/'.$s->icon}}" alt="{{ $s->name }}"></span> --}}
+        <span class="icon flaticon-heartbeat"></span>
+        <h5><a href="#">{{$s->name}}</a></h5>
+        <div class="text">{{$s->description}}</div>
+    </div>
+</div>
--- resources/views/front/services.blade.php
+++ resources/views/front/services.blade.php
@@ -23,14 +23,7 @@
         <div class="row">
             @if (count($service)>0)
                 @foreach ($service as $s)
-                    <div class="service-block col-lg-4 col-md-6 col-sm-12">
-                        <div class="inner-box">
-                            {{-- <span class=""><img src="{{asset('upload/service').'/'.$s->icon}}" alt="{{ $s->name }}"></span> --}}
-                            <span class="icon flaticon-heartbeat"></span>
-                            <h5><a href="#">{{$s->name}}</a></h5>
-                            <div class="text">{{$s->description}}</div>
-                        </div>
-                    </div>
+                  @include('front.components.services')
                 @endforeach
             @endif
         </div>
