ed740ffd35921b31b26fb8e16001c7aa65677f17
Muhammad Hamza Munir <hamzamuniransari@gmail.com>
2023-09-15 11:46:48 +0500

    update

--- resources/views/front/doctorlist.blade.php
+++ resources/views/front/doctorlist.blade.php
@@ -18,38 +18,15 @@
 </section>
 
 
- <section class="team-section-two alternate alternate2">
+<section class="team-section-two alternate alternate2">
     <div class="auto-container">
         <div class="row">
-            @foreach ($doctor as $d)
-                <div class="team-block-two col-lg-4 col-md-6 col-sm-12 wow fadeInUp">
-                    <div class="inner-box">
-                       <div class="image-box">
-                            <?php
-                                if($d->image){
-                                    $image=asset('upload/doctor')."/".$d->image;
-                                }else{
-                                    $image=asset('upload/profile/profile.png');
-                                }
-                            ?>
-                            <figure class="image"><a href="{{url('doctors/').'/'.$d->slug}}"><img src="{{ $image }}" alt="{{ $d->name }}"></a></figure>
-                            <ul class="social-links">
-                                <li><a href="{{isset($d->facebook_id)?$d->facebook_id:'https://www.facebook.com'}}"><span class="fab fa-facebook-f"></span></a></li>
-                                <li><a href="{{isset($d->twitter_id)?$d->twitter_id:'https://twitter.com/search-home'}}"><span class="fab fa-twitter"></span></a></li>
-                                <li><a href="{{isset($d->instagram_id)?$d->instagram_id:'https://www.instagram.com/?hl=en'}}"><span class="fab fa-instagram"></span></a></li>
-                                <li><a href="{{isset($d->linkdin_id)?$d->linkdin_id:'https://www.linkdine.com/?hl=en'}}"><span class="fab fa-linkedin-in"></span></a></li>
-                            </ul>
-                        </div>
-                        <div class="info-box">
-                            <h5 class="name"><a href="{{url('doctors/').'/'.$d->slug}}">{{ $d->name }}</a></h5>
-                            <span class="">{{substr($d->about_us,0,100)}}</span>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+            @if (count($doctor)>0)
+                @foreach ($doctor as $d)
+                    @include('front.components.doctor')
+                @endforeach
+            @endif
         </div>
-
-        <div class="sec-bottom-text">Don’t hesitate, contact us for better help and services. <a href="#">Explore all Dr. Team</a></div>
     </div>
 </section>
 
